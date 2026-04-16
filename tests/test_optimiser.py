c8021b902440151013c4029cbf7bb15e38969cef
Constance Mahony <mahony@astro.rub.de>
2026-04-16 22:42:01 +0100

    added two more tests

--- tests/test_optimiser.py
+++ tests/test_optimiser.py
@@ -4,3 +4,13 @@ def test_basic_example_with_defaults():
     prices = [3,6,2,5]
     schedule, profit = battery_optimiser(prices)
     assert profit == 9.854999999999999
+
+def test_soc_does_not_exceed_max_storage_volume():
+    prices = [361, -18, 159, 352, 220, 101, 49, 330, 337, 21, 226, 302, 87, 137, 356, 486, 210, 27, -40]
+    schedule, profit = battery_optimiser(prices)
+    assert all(schedule["soc"] <= 4.0)
+
+def test_no_profit_with_flat_prices():
+    prices = [50]*20
+    schedule, profit = battery_optimiser(prices)
+    assert profit == 0
